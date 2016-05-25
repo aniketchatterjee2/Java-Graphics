8b655049491200249b17bba95ecf2e732d2d3296
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2016-05-25 14:03:20 +0530

    completed eventListener

--- src/main/java/com/sensei/EventListener/App.java
+++ src/main/java/com/sensei/EventListener/App.java
@@ -1,14 +1,18 @@
 package com.sensei.EventListener;
 
-import javax.swing.JPanel;
-
 public class App {
 
 	public static void main(String[] args) {
-		
-		Panel panel = new Panel();
-		Frame frame = new Frame( "Event listener", 500, 500, panel );
+	
+		EventListener listener = new EventListener();
+		EventListener.ButtonListener buttonListener = new EventListener.ButtonListener();
+		EventListener.TextListener textListener = new EventListener.TextListener();
+		EventListener.FrameListener frameListener = new EventListener.FrameListener();
+
+		Panel panel = new Panel( buttonListener, textListener );
+		listener.setPanel( panel );
+		Frame frame = new Frame( "Event listener", 500, 500, panel, frameListener );
 		frame.setVisible( true );
 	}
-
+	
 }
--- src/main/java/com/sensei/EventListener/Panel.java
+++ src/main/java/com/sensei/EventListener/Panel.java
@@ -1,22 +1,28 @@
 package com.sensei.EventListener;
 
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.JTextArea;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class Panel extends JPanel{
 	
-	public Panel() {
+	JButton button = new JButton( "Button test" );
+	JTextField textField = new JTextField( 15 );
+	JTextArea textArea = new JTextArea( 15, 20 );
+	
+	public Panel( ActionListener a, KeyListener k ) {
+	
+		textArea.setEditable( false );
 		
-		JButton button = new JButton( "Button test" );
-		JTextField textField = new JTextField( 15 );
-		JTextArea textArea = new JTextArea( 15, 20 );
+		JScrollPane pane = new JScrollPane( textArea );
+		pane.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED );
 		
+		button.addActionListener( a );
+		 
+		textField.addKeyListener( k );
 		
 		this.add( button );
 		this.add( textField );
-		this.add( textArea );
+		this.add( pane );
 	}
-
+	
 }
