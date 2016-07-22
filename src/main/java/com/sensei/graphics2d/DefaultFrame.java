3963524f21314d103adb4c5c46e920e39d36d2ee
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2016-07-23 00:22:28 +0530

    new content

--- src/main/java/com/sensei/graphics2d/DefaultFrame.java
+++ src/main/java/com/sensei/graphics2d/DefaultFrame.java
@@ -1,21 +1,27 @@
 package com.sensei.graphics2d;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+
 import javax.swing.JFrame;
 
 public class DefaultFrame extends JFrame{
 
-	private static Graphics2D_01 g1 = null;
-	
 	public DefaultFrame() {
-		g1 = new Graphics2D_01();
+		setUpUI() ;
 		super.setSize( 400 , 400 );
 		super.setVisible( true );
 		super.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-		super.add( g1 );
+	}
+	
+	private void setUpUI() {
+		Container contentPane = super.getContentPane() ;
+		contentPane.setLayout( new BorderLayout() ) ;
+		contentPane.add( new Graphics2D_01() ) ;
 	}
 	
 	public static void main(String[] args) {
-		new DefaultFrame().add(g1);
+		new DefaultFrame();
 	}
 
 }
--- src/main/java/com/sensei/graphics2d/Graphics2D_01.java
+++ src/main/java/com/sensei/graphics2d/Graphics2D_01.java
@@ -8,7 +8,8 @@ import javax.swing.JPanel;
 public class Graphics2D_01 extends JPanel{
 	
 	@Override
-	public void paintComponent( Graphics g ) {
+	public void paint( Graphics g ) {
+		System.out.println( "Paint called." ) ;
 		g.setColor( Color.ORANGE );
 		g.fillRect(50, 50, 100, 100);
 		g.drawLine( 34 , 34, 50, 50);
