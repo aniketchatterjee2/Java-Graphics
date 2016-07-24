6423eff22a8a34d96a810b5b009f4ea6325f03a0
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2016-07-24 09:58:35 +0530

    new content

--- src/main/java/com/sensei/graphics2d/DefaultFrame.java
+++ src/main/java/com/sensei/graphics2d/DefaultFrame.java
@@ -6,22 +6,43 @@ import java.awt.Container;
 import javax.swing.JFrame;
 
 public class DefaultFrame extends JFrame{
+	
+	private Graphics2D_01 g2d = new Graphics2D_01();
 
 	public DefaultFrame() {
 		setUpUI() ;
-		super.setSize( 400 , 400 );
-		super.setVisible( true );
-		super.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 	}
 	
 	private void setUpUI() {
 		Container contentPane = super.getContentPane() ;
 		contentPane.setLayout( new BorderLayout() ) ;
-		contentPane.add( new Graphics2D_01() ) ;
+		contentPane.add( g2d ) ;
+		
+		super.setSize( 400 , 400 );
+		super.setVisible( true );
+		super.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+	}
+	
+	private void go() {
+		int x=0, y=0;
+		
+		for( int i=0; i<200; i++ ) {
+			g2d.setXY( x, y );
+			g2d.repaint();
+			x++;
+			y++;
+			
+			try {
+				Thread.sleep( 20 );
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
-		new DefaultFrame();
+		DefaultFrame frame = new DefaultFrame();
+		frame.go();
 	}
 
 }
